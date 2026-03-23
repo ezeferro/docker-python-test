7854ea0176aab406a6fc2195ae8e8590d6d0f598
Ezequiel Ferro <42ezequiel@gmail.com>
2026-03-23 18:36:46 -0300

    add jinja2 and styles

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/website")
+def website(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
